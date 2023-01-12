fca498e8d6b45511e728075b1840efd2502fac2f
Johanna Liu <johanna.liu@mths.ca>
2023-01-12 18:15:16 +0000

    done

--- rounding.py
+++ rounding.py
@@ -6,11 +6,13 @@
 
 import math
 
+
 def decimal_converter(decimal, decimal_places):
     # rounds a number
     decimal[0] = decimal[0] * pow(10, decimal_places) + 0.5
     decimal[0] = int(decimal[0])
     decimal[0] = decimal[0] / pow(10, decimal_places)
+
 
 def main():
     # takes user input, passes it to functions and calls them
@@ -18,13 +20,15 @@ def main():
 
     print("This program rounds a number.")
     str_temp_decimal = input("Enter a number you want to round: ")
-    str_decimal_places = input("Enter the number of decimal places you want rounded: ")
-    
+    str_decimal_places = input(
+        "Enter the number of decimal places you want to be rounded: "
+    )
+
     try:
         temp_decimal = float(str_temp_decimal)
         decimal.append(temp_decimal)
         decimal_places = int(str_decimal_places)
-        
+
         decimal_converter(decimal, decimal_places)
 
         print("The number rounded is: {0}".format(decimal[0]))
